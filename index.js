43eab14b2cddba3cce2eb6cacf780c22bc7911ec
Dan <daniel.phillip.murray@gmail.com>
2020-04-19 23:26:44 -0500

    added horizontal scrollbar to improve web experience and added 2020-4-17 to 2020-4-19

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const gradient = (vector1, vector2, scalar) => {
   });
 }
 
-const color = (stateName, date, isLatest) => {
+const color = (stateName, date) => {
   const state = window.state_states[stateName];
   if (!state) {
     return unknown;
@@ -64,28 +64,80 @@ const numberOfDays = () => {
 }
 
 const indexDate = (dateZero, index) => {
-  const date = new Date(dateZero);
-  date.setDate(date.getDate() + Number(index));
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate()
-  return date.toISOString().slice(0, 10);
+  const date = new Date(`${dateZero}T00:00:00.000Z`);
+  date.setTime(date.getTime() + index * 24 * 3600 * 1000);
+  return date;
+}
+
+const Months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+const renderDate = (date) => {
+  const wrapperDiv = document.createElement("div");
+  wrapperDiv.className = 'dateWrapper';
+
+  const dateDiv = document.createElement("div");
+  dateDiv.className = 'date';
+  wrapperDiv.appendChild(dateDiv);
+
+  const monthDiv = document.createElement("div");
+  monthDiv.className = 'month';
+  const month = Months[date.getUTCMonth()];
+  const monthText = document.createTextNode(month);
+  monthDiv.appendChild(monthText);
+  dateDiv.appendChild(monthDiv);
+
+  const dayDiv = document.createElement("div");
+  dayDiv.className = 'day';
+  const dayText = document.createTextNode(date.getUTCDate());
+  dayDiv.appendChild(dayText);
+  dateDiv.appendChild(dayDiv);
+  return wrapperDiv;
+}
+
+const renderDateSlider = (dateZero, numDays) => {
+  const dates = Array(numDays).fill().map((_, index) => {
+    const date = new Date(`${dateZero}T00:00:00.000Z`);
+    date.setTime(date.getTime() + index * 24 * 3600 * 1000);
+    return date;
+  });
+
+  const dateSlider = document.getElementById("dateSlider");
+  dates.forEach((date, i) => {
+    const dateDiv = renderDate(date);
+    dateSlider.appendChild(dateDiv);
+  });
+
+  dateSlider.scrollLeft = dateSlider.scrollWidth;
+  return dateSlider;
 }
 
 window.main = () => {
-  const slider = document.getElementById("slider");
-  const dateDiv = document.getElementById("date");
   const backButton = document.getElementById("back");
+  const currentMonthDiv = document.getElementById("currentMonth");
+  const currentDateDiv = document.getElementById("currentDate");
   const dayZero = firstDate();
   const numDays = numberOfDays();
-  slider.max = numDays
-  slider.value = numDays;
-  const displayDate = indexDate(dayZero, numDays);
-  dateDiv.innerText = displayDate;
+  let currentDayIndex = numDays - 1;
+  const currentDate = indexDate(dayZero, currentDayIndex);
+  const currentDateString = currentDate.toISOString().slice(0, 10);
+  currentMonthDiv.innerText = Months[currentDate.getMonth()];
+  currentDateDiv.innerText = currentDate.getUTCDate();
 
-  window.addEventListener('scroll', (e) => {
-    console.log(e);
-  })
+  // console.log(numDays);
+  // console.log(currentDayIndex);
+  // console.log(currentDate.getUTCDate());
 
   const width = 975;
   const height = 610;
@@ -104,6 +156,7 @@ window.main = () => {
 
   const g = svg.append("g");
 
+
   const states = topojson.feature(us, us.objects.states).features;
   states.forEach((state, i) => {
     g.append("g")
@@ -113,7 +166,7 @@ window.main = () => {
     .selectAll("path")
     .data([state])
     .join("path")
-      .attr("fill", color(state.properties.name, displayDate, true))
+      .attr("fill", color(state.properties.name, currentDateString))
       .on("click", clicked)
       .attr("d", path)
     .append("title")
@@ -127,15 +180,37 @@ window.main = () => {
       .attr("stroke-linejoin", "round")
       .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)));
 
-  slider.style.display = 'block';
   // svg.call(zoom);
 
-  slider.oninput = (e) => {
-    const day = e.target.value;
-    const date = indexDate(dayZero, day);
-    dateDiv.innerText = date;
+  const dateSlider = renderDateSlider(dayZero, numDays - 1);
+  let scrollWidth = dateSlider.scrollWidth;
+  let dayWidth = scrollWidth / (numDays + 9)
+
+  window.onresize = (e) => {
+    scrollWidth = dateSlider.scrollWidth;
+    dayWidth = scrollWidth / (numDays + 9);
+    const scrollLeft = dayWidth *  (currentDayIndex)
+    dateSlider.scrollLeft = scrollLeft;
+  }
+
+  dateSlider.onscroll = (e) => {
+    const scrollLeft = e.target.scrollLeft;
+    const dayIndex = Math.round(scrollLeft / dayWidth);
+
+    if (dayIndex === currentDayIndex) {
+      return;
+    }
+
+    currentDayIndex = dayIndex;
+    const date = indexDate(dayZero, dayIndex);
+    const dateString = date.toISOString().slice(0, 10);
+
+    currentMonthDiv.innerText = Months[date.getUTCMonth()];
+    currentDateDiv.innerText = date.getUTCDate();
+
     d3.selectAll("g > g").remove()
     const states = topojson.feature(us, us.objects.states).features;
+
     states.forEach((state, i) => {
       g.append("g")
         .attr("fill", "#444")
@@ -144,7 +219,7 @@ window.main = () => {
       .selectAll("path")
       .data([state])
       .join("path")
-        .attr("fill", color(state.properties.name, date, date === displayDate))
+        .attr("fill", color(state.properties.name, dateString))
         .on("click", clicked)
         .attr("d", path)
       .append("title")
